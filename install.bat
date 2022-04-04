ad140a525c890a5be58f774fc5ae6e0ccec03ed4
alagyn <benkimbrough44@gmail.com>
2022-04-04 19:19:17 -0400

    Updates to install scripts

--- install.bat
+++
@@ -1,6 +0,0 @@
-py -m venv venv
-
-venv\Scripts\pip.exe install --upgrade pip
-venv\Scripts\pip.exe install -r python_reqs.txt
-
-cmd /k
---
+++ install_Windows.bat
@@ -0,0 +1,6 @@
+py -m venv venv
+
+venv\Scripts\pip.exe install --upgrade pip
+venv\Scripts\pip.exe install -r python_reqs.txt
+
+cmd /k
