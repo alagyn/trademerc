fc1ef7b47b427c02ddacc68c5bc147ea16966ece
alagyn <benkimbrough44@gmail.com>
2022-04-04 19:21:27 -0400

    Fixes to linux install script

--- install_Linux.sh
+++ install_Linux.sh
@@ -1,4 +1,4 @@
 python3 -m venv venv
 
-venv/Scripts/pip.exe install --upgrade pip
-venv/Scripts/pip.exe install -r python_reqs.txt
+venv/bin/pip.exe install --upgrade pip
+venv/bin/pip.exe install -r python_reqs.txt
