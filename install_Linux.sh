631bda8e55dd5f26e4e36eb7ffa714b6b035cb0b
alagyn <benkimbrough44@gmail.com>
2022-04-04 22:00:32 -0400

    Updates to Linux Scripts

--- install_Linux.sh
+++ install_Linux.sh
@@ -1,5 +1,8 @@
 python3 -m venv venv
 
+sudo apt-get update
+sudo apt-get install libjpeg-dev
+
 venv/bin/pip install --upgrade pip
 venv/bin/pip install wheel
 venv/bin/pip install -r python_reqs.txt
---
+++ run_gui_linux.sh
@@ -0,0 +1,1 @@
+venv/bin/python backtester_gui.py
