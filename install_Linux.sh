8c0e8425338b961c54736c1c7ec6f45749a5d056
alagyn <benkimbrough44@gmail.com>
2023-01-05 23:50:53 -0500

    update scripts

--- install_Linux.sh
+++ install_Linux.sh
@@ -1,8 +1,8 @@
 python3 -m venv venv
 
 sudo apt-get update
-sudo apt-get install libjpeg-dev
+sudo apt-get install libjpeg-dev libatlas-base-dev
 
 venv/bin/pip install --upgrade pip
 venv/bin/pip install wheel
-venv/bin/pip install -r python_reqs.txt
+venv/bin/pip install -r requirements.txt
---
+++ run_strategy_maker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+venv/bin/python -m "cash_money.strategy_gui"
