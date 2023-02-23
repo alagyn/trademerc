7f12f4c2d674eaa5c08d509671d095ad5888c928
alagyn <benkimbrough44@gmail.com>
2023-02-23 12:54:13 -0500

    filter out duplicate symbols

--- cash_money/utils/file_utils.py
+++ cash_money/utils/file_utils.py
@@ -1,19 +1,19 @@
 import json
+from typing import List
 
 
-def loadStockFile(file):
-    stocks = []
+def loadStockFile(file) -> List[str]:
+    stocks = set()
     with open(file, mode='r') as f:
         lines = [x.strip() for x in f.readlines()]
         for x in lines:
             if len(x) > 0 and not x.startswith('#'):
-                stocks.append(x)
+                stocks.add(x)
 
-    return stocks
+    return sorted(list(stocks))
 
 
 def loadStratFile(file):
     with open(file, mode='r') as f:
         strat = json.load(f)
         return strat
-
