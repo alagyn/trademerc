60188f5b2fbba694452e4c55d488a49c16c30db1
alagyn <benkimbrough44@gmail.com>
2024-05-15 20:05:14 -0400

    Fix other scripts

--- scripts/run_strategy_maker.bat
+++ scripts/run_strategy_maker.bat
@@ -1,6 +1,6 @@
 :: batch magic to get cur dir
 set SCRIPT_DIR=%~dp0
-cd SCRIPT_DIR\..
+cd %SCRIPT_DIR%\..
 
 venv\Scripts\python.exe -m cash_money.strategy_gui
 
--- scripts/run_tests.bat
+++ scripts/run_tests.bat
@@ -1,5 +1,5 @@
 :: batch magic to get cur dir
 set SCRIPT_DIR=%~dp0
-cd SCRIPT_DIR\..
+cd %SCRIPT_DIR%\..
 
 venv\Scripts\python.exe -m unittest discover -v -s .\tests -p test_*.py
